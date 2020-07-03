f7c12a8c7efd27aed8fe1218737678ddf7fc38c7
pabloprogramando <67450712+pabloprogramando@users.noreply.github.com>
2020-07-03 02:08:15 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@ using namespace std;
 
 int main(){
 
-int  opcion, puntajeMaximo;
+int  opcion, puntajeMaximo=0, puntaje1, puntaje2;
+bool bandjuego1=false;
 
 cout<<"                           BIENVENIDO AL JUEGO DE LA GENERALA "<<endl;
 
@@ -20,9 +21,10 @@ while(true){
 
     system("cls");
     cout<<"MENÚ DEL JUEGO"<<endl;
-    cout<<"1- JUEGA UN JUGADOR "<<endl;
-    cout<<"2- JUEGAN DOS JUGADORES "<<endl;
-    cout<<"3- SALIR"<<endl;
+    cout<<"1- JUEGO NUEVO: UN JUGADOR "<<endl;
+    cout<<"2- JUEGO NUEVO: DOS JUGADORES "<<endl;
+    cout<<"3- PUNTUACIÓN MÁS ALTA"<<endl;
+    cout<<"4- SALIR"<<endl;
     cout<<"ELIJA UNA OPCIÓN "<<endl;
     cin>>opcion;
     rlutil::anykey();
@@ -31,14 +33,39 @@ while(true){
     switch(opcion){
 
         case 1:
-            juego1jugador();
+            puntaje1=juego1jugador();
+            if (bandjuego1==false){
+                puntajeMaximo=puntaje1;
+            }
+            else {
+                if (puntaje1>puntajeMaximo){
+                    puntajeMaximo=puntaje1;
+                }
+            }
+            bandjuego1=true;
+
             break;
 
         case 2:
             juego2jugadores();
+            puntaje2=juego1jugador();
+            if (bandjuego1==false){
+                puntajeMaximo=puntaje2;
+            }
+            else {
+                if (puntaje1>puntajeMaximo){
+                    puntajeMaximo=puntaje2;
+                }
+            }
+            bandjuego1=true;
+
             break;
 
         case 3:
+            cout<<"La puntuación más alta es de: "<<puntajeMaximo<<endl;
+            break;
+
+        case 4:
             return 0;
             break;
 
